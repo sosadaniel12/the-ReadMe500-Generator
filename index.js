190c59763f5f2fd9beb1bbd53b6cc4cd7a04b2a1
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-09-24 14:03:09 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is the Title of your readme?",
+      name: "titleName",
+      validate: (title) => {
+        if (title) {
+          return true;
+        } else {
+          return "Please enter a title name";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "How will your application be installed?",
+      name: "installation",
+      validate: (install) => {
+        if (install) {
+          return true;
+        } else {
+          return "Please enter installation instructions to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "How will the application be used?",
+      name: "usageInfo",
+      validate: (usage) => {
+        if (usage) {
+          return true;
+        } else {
+          return "Please enter the usage to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What are contributing guidelines?",
+      name: "contributingGuide",
+      validate: (contribute) => {
+        if (contribute) {
+          return true;
+        } else {
+          return "Please enter contributing guidelines to continue";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What are the test instructions?",
+      name: "testInstructions",
+      validate: (test) => {
+        if (test) {
+          return true;
+        } else {
+          return "Please enter test instructions";
+        }
+      },
+    },
+  ])
+
+  .then((data) => {});
